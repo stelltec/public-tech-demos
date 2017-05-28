343988e94a4f35545a835360bf263bfcf86bbdb3
remojansen <remo.jansen@wolksoftware.com>
2017-05-28 15:19:43 +0100

    added demo1

--- nodejs-madrid-meetup/demo1/src/example2.ts
+++ nodejs-madrid-meetup/demo1/src/example2.ts
@@ -1,34 +1,28 @@
-namespace example2 {
-
-    interface User {
-        surname: string;
-        age: number;
-    }
-
-    function findUserBySurname(users: User[], surname: string) {
-        return users.filter(u => u.surname === surname);
-    }
-
-    const users1 = [
-        { surname: "Smith", age: 28 },
-        { surname: "Johnson", age: 55 },
-        { surname: "Williams", age: 14 }
-    ];
-
-    findUserBySurname(users1, "Smith"); //  [{ surname: "Smith", age: 28 }]
-
-    /*
-    
-    Error: Property 'surname' is missing in type '{ familyName: string; age: string; }'
-
-    const users2 = [
-        { familyName: "Smith", age: 28 },
-        { familyName: "Johnson", age: 55 },
-        { familyName: "Williams", age: 14 }
-    ];
-
-    findUserBySurname(users2, "Smith");
-
-    */
-
+interface User {
+    surname: string;
+    age: number;
 }
+
+function findUserBySurname(users: User[], surname: string) {
+    return users.filter(u => u.surname === surname);
+}
+
+const users1 = [
+    { surname: "Smith", age: 28 },
+    { surname: "Johnson", age: 55 },
+    { surname: "Williams", age: 14 }
+];
+
+findUserBySurname(users1, "Smith"); //  [{ surname: "Smith", age: 28 }]
+
+const users2 = [
+    { familyName: "Smith", age: 28 },
+    { familyName: "Johnson", age: 55 },
+    { familyName: "Williams", age: 14 }
+];
+
+// Error: Property 'surname' is missing in type '{ familyName: string; age: string; }'
+findUserBySurname(users2, "Smith");
+
+var a = null;
+export { a };
